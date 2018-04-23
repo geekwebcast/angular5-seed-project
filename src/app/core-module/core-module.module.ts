21241025ae6f1c3d6c2837dbe3215b483de544ad
NANDA KUMAR <nandakumar.r@perfomatix.com>
2018-04-24 02:20:06 +0530

    basic module created

---
+++ src/app/core-module/core-module.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CoreModuleRoutingModule } from '@app/core-module/core-module-routing.module';
+
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    CoreModuleRoutingModule
+  ],
+  declarations: []
+})
+export class CoreModuleModule { }
---
+++ src/app/shared-module/shared-module.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModuleRoutingModule } from '@app/shared-module/shared-module-routing.module';
+
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    SharedModuleRoutingModule
+  ],
+  declarations: []
+})
+export class SharedModuleModule { }
